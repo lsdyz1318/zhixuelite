92b1ac7b1891b343103be0f05bde2dee0699dbf4
luiooiul <mail@luiooiul.com>
2024-05-27 19:56:48 +0800

    代码优化

--- core/model/src/main/kotlin/com/zhixue/lite/core/model/ReportInfo.kt
+++ core/model/src/main/kotlin/com/zhixue/lite/core/model/ReportInfo.kt
@@ -1,8 +1,8 @@
 package com.zhixue.lite.core.model
 
 data class ReportInfo(
-    val id: String,
-    val name: String,
+    val reportId: String,
+    val reportName: String,
     val dateTime: String,
     val isPublished: Boolean
 )
