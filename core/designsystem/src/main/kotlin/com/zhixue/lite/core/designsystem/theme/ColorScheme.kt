3ab142fa0187925998d618453ce617be19f640e8
luiooiul <mail@luiooiul.com>
2024-06-03 13:16:22 +0800

    优化

--- core/designsystem/src/main/kotlin/com/zhixue/lite/core/designsystem/theme/ColorScheme.kt
+++ core/designsystem/src/main/kotlin/com/zhixue/lite/core/designsystem/theme/ColorScheme.kt
@@ -17,27 +17,29 @@ class ColorScheme(
     val error: Color
 )
 
-internal val lightColorScheme: ColorScheme = ColorScheme(
-    primary = Color(0xFF15D299),
-    onPrimary = Color(0xFFFFFFFF),
-    container = Color(0xFFF4F6F8),
-    background = Color(0xFFFFFFFF),
-    onBackground = Color(0xFF000000),
-    onBackgroundVariant = Color(0xFFCCCCCC),
-    outline = Color(0xFFF8F8F8),
-    error = Color(0xFFE84545)
-)
+internal val lightColorScheme: ColorScheme =
+    ColorScheme(
+        primary = Color(0xFF15D299),
+        onPrimary = Color(0xFFFFFFFF),
+        container = Color(0xFFF4F6F8),
+        background = Color(0xFFFFFFFF),
+        onBackground = Color(0xFF000000),
+        onBackgroundVariant = Color(0xFFCCCCCC),
+        outline = Color(0xFFF8F8F8),
+        error = Color(0xFFE84545)
+    )
 
-internal val darkColorScheme: ColorScheme = ColorScheme(
-    primary = Color(0xFF13C08C),
-    onPrimary = Color(0xFFFFFFFF),
-    container = Color(0xFF212121),
-    background = Color(0xFF141414),
-    onBackground = Color(0xFFFFFFFF),
-    onBackgroundVariant = Color(0xFF707070),
-    outline = Color(0xFF212121),
-    error = Color(0xFFFD4438)
-)
+internal val darkColorScheme: ColorScheme =
+    ColorScheme(
+        primary = Color(0xFF13C08C),
+        onPrimary = Color(0xFFFFFFFF),
+        container = Color(0xFF212121),
+        background = Color(0xFF141414),
+        onBackground = Color(0xFFFFFFFF),
+        onBackgroundVariant = Color(0xFF707070),
+        outline = Color(0xFF212121),
+        error = Color(0xFFFD4438)
+    )
 
 internal val LocalColorScheme: ProvidableCompositionLocal<ColorScheme> =
     staticCompositionLocalOf { lightColorScheme }
